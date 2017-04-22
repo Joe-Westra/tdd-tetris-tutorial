5c5c4e92d146733725fd7cdcc842b4235a80259e
joe <joetheguy@msn.com>
2017-04-22 13:47:12 -0700

    Totally broke from TDD conventions and added the GUI.

--- src/main/java/tetris/ScoreBoard.java
+++ src/main/java/tetris/ScoreBoard.java
@@ -2,14 +2,26 @@
 
 package tetris;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by jdub on 14/04/17.
  */
-public class ScoreBoard {
+public class ScoreBoard extends JLabel{
     private int score;
 
-    public void increaseScore(int amount){
+    ScoreBoard(){
+        this.setBackground(Color.BLACK);
+        this.setBorder(BorderFactory.createLineBorder(Color.GRAY,5,true));
+        this.setText(String.valueOf(score) + " pts");
+        this.setForeground(Color.LIGHT_GRAY);
+        this.setOpaque(true);
+    }
+
+    public void increaseScore(int amount) {
         score += amount;
+        this.setText(String.valueOf(score) + " pts");
     }
 
     public int getScore() {
